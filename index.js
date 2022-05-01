6ce27887f3ffcacbde7b65959b7a75eaffaa5d7c
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-04-30 22:51:11 -0600

    commit tree 7770e1a897c469db1edb4e653e437593709281de

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Yitzchak is the champion"
+document.body.append(newHeader);
